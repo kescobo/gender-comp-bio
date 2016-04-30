32ca2e47f8ae134b41a1923bf8da54bd87ca1b15
Kevin Bonham, PhD <kevbonham@gmail.com>
2016-04-30 16:31:47 -0400

    using genderize.io for more genders

--- src/xml_parsing.py
+++ src/xml_parsing.py
@@ -1,8 +1,10 @@
 import os
+import datetime
 from lxml.etree import iterparse
 import pandas as pd
+from genderize import Genderize
 
-
+os.listdir()
 os.chdir("../data/")
 
 class Article(object):
@@ -137,8 +139,6 @@ def get_unique_names(xml_file, name_set=None):
     if not name_set:
         name_set = set([])
 
-    if len(new_names) % 1000 == 0:
-        print("made it through {}".format(len(new_names)))
     for article in iter_parse_pubmed(xml_file):
         first, last, second, penultimate, others = score_authors(article.authors)
 
@@ -166,3 +166,53 @@ def get_unique_names(xml_file, name_set=None):
         new_names.extend([fa, la, sa, pa, *oa])
 
     return name_set.union(set(new_names))
+
+unique_names = get_unique_names("/Users/KBLaptop/computation/gender-comp-bio/data/biology-1997-2014.xml", unique_names)
+len(unique_names)
+
+fixed_names = []
+for name in unique_names:
+    if name:
+        if len(name) > 1:
+            fixed_names.append(name)
+fixed_names.sort()
+print(len(fixed_names))
+
+genders = []
+
+os.chdir("../src")
+from genderize_key import my_key
+genderize = Genderize(
+    user_agent='Kevin_Bonham',
+    api_key=my_key)
+
+for i in range(0, len(fixed_names), 10):
+    query = fixed_names[i:i+10]
+    genders.extend(genderize.get(query))
+
+genders
+with open("../data/genderize_data.txt", "w+") as outfile:
+    for gender in genders:
+        outfile.write(str(gender))
+        outfile.write("\n")
+
+names_dict = {}
+
+for gender in genders:
+    n = gender["name"]
+    g = gender["gender"]
+    if g != None:
+        p = gender["probability"]
+        c = gender["count"]
+    else:
+        p = None
+        c = None
+    n = gender["name"]
+    g = gender["gender"]
+
+    names_dict[n] = {"gender":g, "probability":p, "count": c}
+
+import json
+
+with open("../data/gender_dict.json", "w+") as outfile:
+    outfile.write(json.dumps(names_dict, indent=4))
